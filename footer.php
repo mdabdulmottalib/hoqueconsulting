309f6ffbe0f16115451398313d84192de0dd3d18
Abdul Musabbir <musabbir.net@gmail.com>
2024-11-16 01:54:38 +0600

    Header and footer created

---
+++ footer.php
@@ -0,0 +1,356 @@
+
+    <!-- rts footer area start -->
+    <div class="rts-footer-area-one pt--80 pb--80">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <!-- footer top area -->
+            <div class="footer-top-area">
+              <div class="logo-area">
+                <a href="#">
+                  <img
+                    class="light"
+                    src="assets//images/logo/logo-1.svg"
+                    alt="logo"
+                  />
+                  <img
+                    class="dark"
+                    src="assets//images/logo/logo-one-dark.svg"
+                    alt="logo"
+                  />
+                </a>
+              </div>
+              <ul class="social-area-one">
+                <li>
+                  <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </li>
+                <li>
+                  <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                </li>
+              </ul>
+            </div>
+            <!-- footer top area end -->
+          </div>
+        </div>
+        <div class="row g-48 g-lg-0 pt--40">
+          <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+            <div class="single-footer-wized-one">
+              <h6 class="title">Solutions</h6>
+              <ul>
+                <li><a href="#">Content Marketing</a></li>
+                <li><a href="#">Consumer Insights</a></li>
+                <li><a href="#">SEO Content Strategy</a></li>
+                <li><a href="#">Keyword Research</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+            <div class="single-footer-wized-one">
+              <h6 class="title">Resources</h6>
+              <ul>
+                <li><a href="#">Insights</a></li>
+                <li><a href="#">Keyword Generator</a></li>
+                <li><a href="#">Research Tool</a></li>
+                <li><a href="#">News & Events</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+            <div class="single-footer-wized-one">
+              <h6 class="title">Company</h6>
+              <ul>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+            <div class="single-footer-wized-one">
+              <h6 class="title">Other links</h6>
+              <ul>
+                <li><a href="#">Content Marketing</a></li>
+                <li><a href="#">Consumer Insights</a></li>
+                <li><a href="#">SEO Content Strategy</a></li>
+                <li><a href="#">Keyword Research</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <!-- footer input wrapper inbner-main area -->
+            <div class="footer-input-main-wrapper-one">
+              <div class="single-footer-wized-one">
+                <h6 class="title">Newsletter</h6>
+                <form action="#">
+                  <input type="email" placeholder="Email address.." required />
+                  <i class="fa-regular fa-envelope"></i>
+                  <button class="rts-btn btn-primary">Subscribe now</button>
+                </form>
+              </div>
+            </div>
+            <!-- footer input wrapper inbner-main area end -->
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <!-- copyright area start -->
+            <div class="copyright-area-start">
+              <p>© 2023 Fluxi, All right reserved.</p>
+              <ul>
+                <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                <li><a href="terms.html">Terms of service</a></li>
+              </ul>
+            </div>
+            <!-- copyright area end -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- rts footer area end -->
+
+    <div id="anywhere-home" class=""></div>
+
+    <!-- side bar area  -->
+    <div id="side-bar" class="side-bar header-two">
+      <button class="close-icon-menu">
+        <i class="fa-sharp fa-thin fa-xmark"></i>
+      </button>
+      <!-- mobile menu area start -->
+      <div class="mobile-menu-main">
+        <nav class="nav-main mainmenu-nav mt--30">
+          <ul class="mainmenu metismenu" id="mobile-menu-active">
+            <li class="has-droupdown">
+              <a href="#" class="main">Home</a>
+              <ul class="submenu mm-collapse">
+                <li>
+                  <a class="mobile-menu-link" href="index.html"
+                    >SEO Marketing</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="index-two.html"
+                    >Digital Marketing</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="index-three.html"
+                    >Digital Agency</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="index-four.html"
+                    >Startup Agency</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="index-five.html"
+                    >Advertising Agency</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="index-six.html"
+                    >Web Design Agency</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="index-seven.html"
+                    >Social Media Agency</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="index-eight.html"
+                    >SEO Agency</a
+                  >
+                </li>
+              </ul>
+            </li>
+            <li class="has-droupdown">
+              <a href="#" class="main">Pages</a>
+              <ul class="submenu mm-collapse">
+                <li><a class="mobile-menu-link" href="about.html">About</a></li>
+                <li>
+                  <a class="mobile-menu-link" href="team.html">Our Team</a>
+                </li>
+                <li><a class="mobile-menu-link" href="faq.html">FAQ's</a></li>
+                <li>
+                  <a class="mobile-menu-link" href="book-a-demo.html"
+                    >Book a Demo</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="free-audit.html"
+                    >Free Audit</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="pricing.html"
+                    >Pricing Plans</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="404.html">404 Page</a>
+                </li>
+              </ul>
+            </li>
+            <li class="has-droupdown">
+              <a href="#" class="main">Services</a>
+              <ul class="submenu mm-collapse">
+                <li>
+                  <a class="mobile-menu-link" href="service.html">Services</a>
+                </li>
+                <li class="has-droupdown third-lvl">
+                  <a class="main" href="#">Service Details</a>
+                  <ul class="submenu-third-lvl mm-collapse">
+                    <li>
+                      <a href="service-details.html"></a>Paid Search Marketing
+                    </li>
+                    <li><a href="email-marketing.html"></a>Email Marketing</li>
+                    <li>
+                      <a href="social-media-marketing.html"></a>Social Media
+                      Marketing
+                    </li>
+                    <li>
+                      <a href="influencer-marketing.html"></a>Influencer
+                      Marketing
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li class="has-droupdown">
+              <a href="#" class="main">Work</a>
+              <ul class="submenu mm-collapse">
+                <li>
+                  <a class="mobile-menu-link" href="case-studies.html"
+                    >Case Studies</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="single-case-studies.html"
+                    >Case Studies Details</a
+                  >
+                </li>
+              </ul>
+            </li>
+            <li class="has-droupdown">
+              <a href="#" class="main">Blog</a>
+              <ul class="submenu mm-collapse">
+                <li><a class="mobile-menu-link" href="blog.html">Blog</a></li>
+                <li>
+                  <a class="mobile-menu-link" href="blog-left-sidebar.html"
+                    >Blog Left Sidebar</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="blog-grid.html"
+                    >Blog Grid</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="blog-grid-2.html"
+                    >Blog Grid Right Sidebar</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="blog-grid-3.html"
+                    >Blog Grid Left Sidebar</a
+                  >
+                </li>
+                <li>
+                  <a class="mobile-menu-link" href="blog-details.html"
+                    >Blog Details</a
+                  >
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="contact.html" class="main">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+
+        <ul class="social-area-one pl--20 mt--100">
+          <li>
+            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+          </li>
+        </ul>
+      </div>
+      <!-- mobile menu area end -->
+    </div>
+    <!-- side abr area end -->
+
+    <!-- pre loader start -->
+    <div class="loader-wrapper">
+      <div class="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+    </div>
+    <!-- pre loader end -->
+
+    <!-- THEME MODE SWITCHER -->
+    <div class="rts-switcher rts-theme-mode">
+      <div class="rts-darkmode">
+        <a id="rts-data-toggle" class="rts-dark-light">
+          <i class="rts-go-dark fal fa-moon"></i>
+          <i class="rts-go-light fa-light fa-sun-bright"></i>
+        </a>
+      </div>
+    </div>
+    <!-- THEME MODE SWITCHER END -->
+
+    <!-- progress area start -->
+    <div class="progress-wrap">
+      <svg
+        class="progress-circle svg-content"
+        width="100%"
+        height="100%"
+        viewBox="-1 -1 102 102"
+      >
+        <path
+          d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
+          style="
+            transition: stroke-dashoffset 10ms linear 0s;
+            stroke-dasharray: 307.919, 307.919;
+            stroke-dashoffset: 307.919;
+          "
+        ></path>
+      </svg>
+    </div>
+    <!-- progress area end -->
+
+    <!-- jquery js -->
+    <script defer src="assets/js/plugins/jquery.min.js"></script>
+    <script defer src="assets/js/plugins/bootstrap.min.js"></script>
+    <script defer src="assets/js/plugins/metismenu.js"></script>
+    <script defer src="assets/js/vendor/jqueryui.js"></script>
+    <script defer src="assets/js/vendor/waypoint.js"></script>
+    <script defer src="assets/js/plugins/swiper.js"></script>
+    <script defer src="assets/js/plugins/theia-sticky-sidebar.min.js"></script>
+    <script defer src="assets/js/plugins/gsap.min.js"></script>
+    <script defer src="assets/js/plugins/scrolltigger.js"></script>
+    <script defer src="assets/js/vendor/split-text.js"></script>
+    <script defer src="assets/js/vendor/split-type.js"></script>
+    <script defer src="assets/js/vendor/waw.js"></script>
+    <script defer src="assets/js/plugins/counter-up.js"></script>
+    <script defer src="assets/js/plugins/magnific-popup.js"></script>
+    <!-- contact form js -->
+    <script defer src="assets/js/plugins/contact-form.js"></script>
+    <script defer src="assets/js/main.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="shortcut icon"
+      type="image/x-icon"
+      href="assets/images/fav.png"
+    />
+    <title>Hoque Consulting - Helps you to scale</title>
+    <!-- swiper css -->
+    <link
+      rel="stylesheet preload"
+      href="assets/css/plugins/swiper.min.css"
+      as="style"
+    />
+    <!-- magnific popup css -->
+    <link
+      rel="stylesheet preload"
+      href="assets/css/plugins/magnific-popup.css"
+      as="style"
+    />
+    <!-- metismenu css -->
+    <link
+      rel="stylesheet preload"
+      href="assets/css/plugins/metismenu.css"
+      as="style"
+    />
+    <!-- bootstrap css -->
+    <link
+      rel="stylesheet preload"
+      href="assets/css/vendor/bootstrap.min.css"
+      as="style"
+    />
+    <!-- fontawesome css -->
+
+    <link
+      rel="stylesheet preload"
+      href="assets/css/plugins/fontawesome.min.css"
+      as="style"
+    />
+    <!-- Custom css -->
+    <link rel="stylesheet preload" href="assets/css/style.css" as="style" />
+  </head>
+
+  <body>
+    <!-- header area start -->
+    <header class="header-style-one header--sticky">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="header-style-one-wrapper">
+              <div class="logo-area">
+                <a href="index.html" class="logo">
+                  <img
+                    class="light"
+                    src="assets/images/logo/logo-1.svg"
+                    alt="logo"
+                  />
+                  <img
+                    class="dark"
+                    src="assets/images/logo/logo-one-dark.svg"
+                    alt="logo"
+                  />
+                </a>
+              </div>
+              <nav class="main-nav-area">
+                <ul class="list-unstyled fluxi-desktop-menu">
+                  <li class="menu-item fluxi-has-dropdown">
+                    <a href="#" class="fluxi-dropdown-main-element">Home</a>
+                    <!-- Start Dropdown Menu -->
+                    <ul class="fluxi-submenu list-unstyled menu-home">
+                      <li class="nav-item">
+                        <a class="nav-link page" href="index.html"
+                          >SEO Marketing</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="index-two.html"
+                          >Digital Marketing</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="index-three.html"
+                          >Digital Agency</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="index-four.html"
+                          >Startup Agency</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="index-five.html"
+                          >Advertising agency</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="index-six.html"
+                          >Web Design Agency</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="index-seven.html"
+                          >Social Media agency</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="index-eight.html"
+                          >SEO Agency</a
+                        >
+                      </li>
+                    </ul>
+                    <!-- End Dropdown Menu -->
+                  </li>
+                  <li class="menu-item fluxi-has-dropdown">
+                    <a href="#" class="fluxi-dropdown-main-element">Pages</a>
+                    <!-- Start Dropdown Menu -->
+                    <ul class="fluxi-submenu list-unstyled menu-home">
+                      <li class="nav-item">
+                        <a class="nav-link page" href="about.html">About</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link page" href="team.html">Our Team</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link page" href="faq.html">FAQ's</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="book-a-demo.html"
+                          >Book a Demo</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="free-audit.html"
+                          >Free Audit</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="pricing.html"
+                          >Pricing Plans</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="404.html">404 Page</a>
+                      </li>
+                    </ul>
+                    <!-- End Dropdown Menu -->
+                  </li>
+                  <li class="menu-item fluxi-has-dropdown">
+                    <a href="#" class="fluxi-dropdown-main-element">Services</a>
+                    <!-- Start Dropdown Menu -->
+                    <ul class="fluxi-submenu list-unstyled menu-home">
+                      <li class="nav-item">
+                        <a
+                          class="nav-link page"
+                          href="service-web-development.html"
+                          >Web Development</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a
+                          class="nav-link page"
+                          href="service-app-development.html"
+                          >App Architecture & Development</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a
+                          class="nav-link page"
+                          href="service-graphic-design.html"
+                          >Graphic Design</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a
+                          class="nav-link page"
+                          href="service-video-editing.html"
+                          >Video Editing</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a
+                          class="nav-link page"
+                          href="service-digital-marketing.html"
+                          >Digital Marketing</a
+                        >
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link page" href="service-seo.html"
+                          >Search Engine Optimization</a
+                        >
+                      </li>
+                    </ul>
+                    <!-- End Dropdown Menu -->
+                  </li>
+
+                  <li class="menu-item fluxi-has-dropdown">
+                    <a href="#" class="fluxi-dropdown-main-element"
+                      >Portfolio</a
+                    >
+                  </li>
+                  <li class="menu-item fluxi-has-dropdown">
+                    <a href="#" class="fluxi-dropdown-main-element">About us</a>
+                  </li>
+                  <li class="menu-item">
+                    <a
+                      class="main-element fluxi-dropdown-main-element"
+                      href="contact.html"
+                      >Contact us</a
+                    >
+                  </li>
+                </ul>
+              </nav>
+              <div class="button-area-start">
+                <a class="call-us" href="tel:123-456-7890"
+                  >Call Us : +134 (99) 865</a
+                >
+                <a href="free-audit.html" class="rts-btn btn-primary"
+                  >Free Audit</a
+                >
+                <div class="menu-btn" id="menu-btn">
+                  <svg
+                    width="20"
+                    height="16"
+                    viewBox="0 0 20 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <rect y="14" width="20" height="2" fill="#1F1F25"></rect>
+                    <rect y="7" width="20" height="2" fill="#1F1F25"></rect>
+                    <rect width="20" height="2" fill="#1F1F25"></rect>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- header area end -->
